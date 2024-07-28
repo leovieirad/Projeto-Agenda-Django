2edc0bc4824e17b8289ae877f66de38d5235ab97
leovieirad <leovieiradsz@gmail.com>
2024-07-28 10:54:36 -0300

    Manipulando QuerySets Django com filter, order_by e fatiamento

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -3,7 +3,11 @@ from contact.models import Contact
 
 
 def index(request):
-    contacts = Contact.objects.all()
+    contacts = Contact.objects \
+        .filter(show=True)\
+        .order_by('-id')[0:10]
+    
+    print(contacts.query)
     
     context = {
         'contacts': contacts,
