342da40af11c3e9e4be01f3263aa113a555eabd0
leovieirad <leovieiradsz@gmail.com>
2024-07-30 12:03:43 -0300

    Criando arquivos parciais para usar com include nos templates

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -10,6 +10,8 @@ def index(request):
         
     context = {
         'contacts': contacts,
+        'site_title': 'Contatos - '
+
     }
 
     return render(
@@ -22,9 +24,11 @@ def contact(request, contact_id):
     # single_contact = Contact.objects.filter(pk=contact_id).first()
     single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
 
-           
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
+               
     context = {
         'contact': single_contact,
+        'site_title': site_title
     }
 
     return render(
