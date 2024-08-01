1305093b23b0c8a2b80383b2d96d952f894a80f2
leovieirad <leovieiradsz@gmail.com>
2024-08-01 16:44:16 -0300

    Usando request.method e request.POST para saber quando o formulário é postado

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -4,9 +4,21 @@ from django.db.models import Q
 from django.core.paginator import Paginator
 
 def create(request):
+    if request.method == 'POST':
+        print()
+        print(request.method)
+        print(request.POST.get('first_name'))
+        print(request.POST.get('last_name'))
+        print()
+
     context = {
 
     }
+
+    print()
+    print(request.method)
+    print()
+    
     return render(
         request,
         'contact/create.html',
