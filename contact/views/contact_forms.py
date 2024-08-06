dcf447d70f911c80bf9c40c49591a823597647ad
leovieirad <leovieiradsz@gmail.com>
2024-08-06 14:44:50 -0300

    Criando a view, url e template para "delete" (apagar contatos)

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -58,4 +58,18 @@ def update(request, contact_id):
         context
     )
 
+def delete(request, contact_id):
+    contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    confirmation = request.POST.get('confirmation', 'no')
+    if confirmation == 'yes':
+        contact.delete()
+        return redirect('contact:index')
 
+    return render(
+        request,
+        'contact/contact.html',
+        {
+            'contact' : contact,
+            'confirmation' : confirmation,
+        }
+    )
