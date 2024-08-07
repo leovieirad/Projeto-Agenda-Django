070c72f210d63ab6cafc90365bb3af32d9d896cb
leovieirad <leovieiradsz@gmail.com>
2024-08-07 11:48:20 -0300

    Trabalhando com ImageField no template e nos forms

--- contact/forms.py
+++ contact/forms.py
@@ -4,39 +4,21 @@ from django import forms
 from . import models
 
 class ContactForm(forms.ModelForm):
-    first_name = forms.CharField(
-        widget=forms.TextInput(
+    picture = forms.ImageField(
+        widget=forms.FileInput(
             attrs={
-                'class': 'classe-a classe_b',
-                'placeholder': 'Escreva aqui',
-
+                'accept': 'image/*',
             }
-        ),
-        label='Primeiro Nome',
-        help_text='Texto de ajuda para seu usuário',
+        )
     )
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        # self.fields['first_name'].widget.attrs.update({
-        #      'class': 'classe-a classe_b',
-        #      'placeholder': 'Escreva aqui',            
-        # })
+    
     class Meta:
         model = models.Contact
         fields = (
             'first_name', 'last_name', 'phone',
             'email', 'description', 'category',
+            'picture',
             )
-        # widgets = {
-        #     'first_name': forms.TextInput(
-        #          attrs={
-        #              'class': 'classe-a classe_b',
-        #              'placeholder': 'Escreva aqui',
-        #          }
-        #     ) 
-        # }
 
     def clean(self):
         cleaned_data = self.cleaned_data
